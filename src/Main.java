17118b31e2c6e4debce9a6925b17fa9921d6b2bf
rjsq <sag19890128@gmail.com>
2021-02-03 19:47:57 +0800

    Add files via upload
    utilize threads for the RPA

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 
 /**
- *
  * @author dell
  */
 public class Main {
@@ -28,11 +27,13 @@ public class Main {
     public static void main(String[] args) throws AWTException {
         // TODO code application logic here
         GlobalKeyboardHook keyboardHook = new GlobalKeyboardHook(true); // Use false here to switch to hook instead of raw input
-        Screen s = new Screen();
-        Robot r = new Robot();
 
+        //initialize silkulix screen, add ImagePath
+        Screen s = new Screen();
         ImagePath.add("src/images");
         ImagePath.add("images");
+
+        //check ImagePath
         List<ImagePath.PathEntry> paths = ImagePath.getPaths();
         for (ImagePath.PathEntry path : paths) {
             System.out.println(path);
@@ -41,50 +42,100 @@ public class Main {
         System.out.println(
                 "Global keyboard hook successfully started, press [escape] key to shutdown. Connected keyboards:");
 
-        keyboardHook.addKeyListener(
-                new GlobalKeyAdapter() {
+        //create thread for global key listener, press ESCAPE to stop the loop
+        (new Thread() {
+            public void run() {
+                keyboardHook.addKeyListener(
+                        new GlobalKeyAdapter() {
 
-                    @Override
-                    public void keyPressed(GlobalKeyEvent event
-                    ) {
-                        System.out.println(event);
-                        if (event.getVirtualKeyCode() == GlobalKeyEvent.VK_ESCAPE) {
-                            run = false;
+                            @Override
+                            public void keyPressed(GlobalKeyEvent event
+                            ) {
+                                System.out.println(event);
+                                if (event.getVirtualKeyCode() == GlobalKeyEvent.VK_ESCAPE) {
+                                    run = false;
+                                }
+                            }
+
+                            @Override
+                            public void keyReleased(GlobalKeyEvent event
+                            ) {
+                                System.out.println(event);
+                            }
                         }
-                    }
+                );
 
-                    @Override
-                    public void keyReleased(GlobalKeyEvent event
-                    ) {
-                        System.out.println(event);
-                    }
-                }
-        );
 
-        try {
-            while (run) {
-
-                try {
-                    while (true) {
-                        s.wait("1.png",3600);
-                        r.delay(1000);
-                        s.click();
-                        s.wait("2.png",3600);
-                        r.delay(1000);
-                        s.click();
-                        s.wait("3.png",3600);
-                        r.delay(1000);
-                        s.click();
-                    }
-                } catch (FindFailed e) {
-                }
             }
-        } catch (Exception e) {
-            //Do nothing
-        } finally {
-            keyboardHook.shutdownHook();
-        }
+        }).start();
 
+        //click 1.png
+        (new Thread() {
+            public void run() {
+
+                Robot r1 = null;
+                try {
+                    r1 = new Robot();
+                    while (run) {
+                        try {
+                            s.wait("1.png", 3000);
+                        } catch (FindFailed findFailed) {
+                            findFailed.printStackTrace();
+                        }
+                        r1.delay(1000);
+                        s.click();
+                    }
+                } catch (AWTException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+        //click 2.png
+        (new Thread() {
+            public void run() {
+
+                Robot r2 = null;
+                try {
+                    r2 = new Robot();
+                    while (run) {
+                        try {
+                            s.wait("2.png", 3000);
+                        } catch (FindFailed findFailed) {
+                            findFailed.printStackTrace();
+                        }
+                        r2.delay(1000);
+                        s.click();
+                    }
+                } catch (AWTException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+        //click 3.png
+        (new Thread() {
+            public void run() {
+                Robot r3 = null;
+                try {
+                    r3 = new Robot();
+                    while (run) {
+                        try {
+                            s.wait("3.png", 3000);
+                        } catch (FindFailed findFailed) {
+                            findFailed.printStackTrace();
+                        }
+                        r3.delay(1000);
+                        s.click();
+                    }
+                } catch (AWTException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
     }
 
 }
